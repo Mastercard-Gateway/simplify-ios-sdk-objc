48828f9dfef82a112905aded92ae0a0fcd1389ef
Adam Axe <adam_axe@mastercard.com>
2014-05-05 20:44:06 -0500

    Upgraded Project to support Framework

--- SimplifyMerchantSDK/SimplifyMerchantSDK/SimplifyMerchantSDK.h
+++ SimplifyMerchantSDK/SimplifyMerchantSDK/SimplifyMerchantSDK.h
@@ -1,13 +1,3 @@
-//
-//  SimplifyMerchantSDK.h
-//  SimplifyMerchantSDK
-//
-//  Created by Adam Axe on 5/5/14.
-//  Copyright (c) 2014 MasterCard Labs. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
 @interface SimplifyMerchantSDK : NSObject
 
 @end
--- SimplifyMerchantSDK/SimplifyMerchantSDK/SimplifyMerchantSDK.m
+++ SimplifyMerchantSDK/SimplifyMerchantSDK/SimplifyMerchantSDK.m
@@ -1,11 +1,3 @@
-//
-//  SimplifyMerchantSDK.m
-//  SimplifyMerchantSDK
-//
-//  Created by Adam Axe on 5/5/14.
-//  Copyright (c) 2014 MasterCard Labs. All rights reserved.
-//
-
 #import "SimplifyMerchantSDK.h"
 
 @implementation SimplifyMerchantSDK
