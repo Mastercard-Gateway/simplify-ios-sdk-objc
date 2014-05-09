99981d3242ab3aaf664ae01a140e3ca5150b6cd8
Neem Serra <neem_serra@mastercard.com>
2014-05-08 19:25:26 -0500

    Merge conflict fix MAPPS-2939

--- SimplifySDKSampleApp/SimplifySDKSampleApp/SIMChargeCardViewController.h
+++ SimplifySDKSampleApp/SimplifySDKSampleApp/SIMChargeCardViewController.h
@@ -1,12 +1,15 @@
-@interface SIMChargeCardViewController : UIViewController <UITextFieldDelegate>
-@property (strong, nonatomic) IBOutlet UITextField *cardNumberField;
-@property (strong, nonatomic) IBOutlet UITextField *expirationField;
-@property (strong, nonatomic) IBOutlet UITextField *cvcField;
-@property (strong, nonatomic) IBOutlet UIButton *chargeCardButton;
-@property (strong, nonatomic) IBOutlet UIImageView *cardTypeImage;
-@property (strong, nonatomic) IBOutlet UIView *cvcCodeView;
-@property (strong, nonatomic) IBOutlet UIView *cardNumberView;
-@property (strong, nonatomic) IBOutlet UIView *expirationDateView;
+#import <Simplify/SIMCreditCardToken.h>
 
+@protocol SIMChargeCardViewControllerDelegate
+
+-(void)checkoutCancelled;
+-(void)requestedCreditCardToken:(SIMCreditCardToken *)token withError:(NSError *)error;
+-(void)requestedPaymentProcess:(NSString *)paymentID withError:(NSError *)error;
+
+@end
+
+@interface SIMChargeCardViewController : UIViewController
+
+@property (nonatomic, weak) id <SIMChargeCardViewControllerDelegate> delegate;
 
 @end
--- SimplifySDKSampleApp/SimplifySDKSampleApp/SIMChargeCardViewController.m
+++ SimplifySDKSampleApp/SimplifySDKSampleApp/SIMChargeCardViewController.m
@@ -2,8 +2,16 @@
 #import "SIMChargeCardViewController.h"
 #import "SIMChargeCardModel.h"
 
-@interface SIMChargeCardViewController ()<SIMChargeCardModelDelegate>
+@interface SIMChargeCardViewController () <SIMChargeCardModelDelegate, UITextFieldDelegate>
 @property (nonatomic, strong) SIMChargeCardModel *chargeCardModel;
+@property (strong, nonatomic) IBOutlet UITextField *cardNumberField;
+@property (strong, nonatomic) IBOutlet UITextField *expirationField;
+@property (strong, nonatomic) IBOutlet UITextField *cvcField;
+@property (strong, nonatomic) IBOutlet UIButton *chargeCardButton;
+@property (strong, nonatomic) IBOutlet UIImageView *cardTypeImage;
+@property (strong, nonatomic) IBOutlet UIView *cvcCodeView;
+@property (strong, nonatomic) IBOutlet UIView *cardNumberView;
+@property (strong, nonatomic) IBOutlet UIView *expirationDateView;
 
 @end
 
