c98080bd10815620efc459f8b7d6f5f2bbd35b91
Neem Serra <neem_serra@mastercard.com>
2014-05-13 14:39:19 -0500

    more documentation and moving things around to make more sense MAPPS-2939

--- SimplifyMerchantSDK/SimplifyMerchantSDK/SIMSDKHelper.h
+++ SimplifyMerchantSDK/SimplifyMerchantSDK/SIMSDKHelper.h
@@ -1,8 +1,20 @@
 #import <UIKit/UIKit.h>
-
+/**
+ * Helper for generating the SDK
+ */
 @interface SIMSDKHelper : NSObject
 
+/**
+ Bundles the framework for Simplify
+ @return a bundled version of the Simplify Merchant SDK
+ */
 +(NSBundle*)frameworkBundle;
+
+/**
+ Obtains the UIImage from an NSString
+ @param name is the file name of the image to be retrieved
+ @return a UIImage
+ */
 +(UIImage*)imageNamed:(NSString*)name;
 
 @end
