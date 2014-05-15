89b62bb9bf053c5afe34547d0bdc1beef9deccfb
Neem Serra <neem_serra@mastercard.com>
2014-05-15 16:39:01 -0500

    changing where delegate is MAPPS-2939

--- SimplifySDKSampleApp/SimplifySDKSampleApp/SIMAppDelegate.m
+++ SimplifySDKSampleApp/SimplifySDKSampleApp/SIMAppDelegate.m
@@ -5,7 +5,7 @@
 #import "SIMProductViewController.h"
 
 
-@interface SIMAppDelegate () <SIMChargeCardViewControllerDelegate>
+@interface SIMAppDelegate () 
 @property (nonatomic, strong) UIStoryboard *storyboard;
 @end
 
@@ -23,51 +23,6 @@
 
     [self.window makeKeyAndVisible];
     return YES;
-}
-
-#pragma mark - SIMChargeViewController Protocol
--(void)chargeCardCancelled {
-    //User cancelled the SIMChargeViewController
-    NSLog(@"User Cancelled");
-}
-
--(void)creditCardTokenFailedWithError:(NSError *)error {
-    
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error processing token" message:error.localizedDescription delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-    
-    [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:NO];
-
-    NSLog(@"Credit Card Token Failed with error:%@", error.localizedDescription);
-}
-
--(void)creditCardTokenProcessed:(SIMCreditCardToken *)token {
-    
-    NSURL *url= [NSURL URLWithString:@"https://Your_server/charge.rb"];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10.0];
-    [request setHTTPMethod:@"POST"];
-    NSString *postString = @"simplifyToken=";
-    
-    postString = [postString stringByAppendingString:token.token];
-    
-    [request setHTTPBody:[postString dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    NSError *error;
-    
-    //Process Request on your own server
-    
-    if (error) {
-        NSLog(@"error:%@", error);
-    } else {
-        
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Success"
-                                                        message:@"Payment Processed Successfully"
-                                                       delegate:self
-                                              cancelButtonTitle:@"OK"
-                                              otherButtonTitles:nil];
-        
-        [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:NO];
-    }
-
 }
 
 @end
--- SimplifySDKSampleApp/SimplifySDKSampleApp/SIMProductViewController.m
+++ SimplifySDKSampleApp/SimplifySDKSampleApp/SIMProductViewController.m
@@ -8,7 +8,7 @@
 
 #import "SIMProductViewController.h"
 #import <Simplify/SIMChargeCardViewController.h>
-@interface SIMProductViewController ()
+@interface SIMProductViewController ()<SIMChargeCardViewControllerDelegate>
 
 @end
 
@@ -35,7 +35,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -47,7 +46,54 @@
     
     SIMChargeCardViewController *chargeController = [segue destinationViewController];
     chargeController =[[SIMChargeCardViewController alloc] initWithApiKey:@"sbpb_N2ZkOGIwZWYtYTg3My00OTE1LWI3ZjgtMzZhMzZhZTAyYTY5"];
+    chargeController.delegate = self;
     // Pass the selected object to the new view controller.
+}
+
+
+#pragma mark - SIMChargeViewController Protocol
+-(void)chargeCardCancelled {
+    //User cancelled the SIMChargeViewController
+    NSLog(@"User Cancelled");
+}
+
+-(void)creditCardTokenFailedWithError:(NSError *)error {
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error processing token" message:error.localizedDescription delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:NO];
+    
+    NSLog(@"Credit Card Token Failed with error:%@", error.localizedDescription);
+}
+
+-(void)creditCardTokenProcessed:(SIMCreditCardToken *)token {
+    
+    NSURL *url= [NSURL URLWithString:@"https://Your_server/charge.rb"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10.0];
+    [request setHTTPMethod:@"POST"];
+    NSString *postString = @"simplifyToken=";
+    
+    postString = [postString stringByAppendingString:token.token];
+    
+    [request setHTTPBody:[postString dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    NSError *error;
+    
+    //Process Request on your own server
+    
+    if (error) {
+        NSLog(@"error:%@", error);
+    } else {
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Success"
+                                                        message:@"Payment Processed Successfully"
+                                                       delegate:self
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        
+        [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:NO];
+    }
+    
 }
 
 @end
