d5f358a92b262978db87893d8cdcd97d846fb430
Neem Serra <neem_serra@mastercard.com>
2014-05-16 14:44:25 -0500

    cupcake

--- SimplifySDKSampleApp/SimplifySDKSampleApp/SIMProductViewController.h
+++ SimplifySDKSampleApp/SimplifySDKSampleApp/SIMProductViewController.h
@@ -1,11 +1,3 @@
-//
-//  SIMProductViewController.h
-//  SimplifySDKSampleApp
-//
-//  Created by Neem Serra on 5/15/14.
-//  Copyright (c) 2014 MasterCard. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface SIMProductViewController : UIViewController
