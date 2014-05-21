74655ee01acc25603c32da65fcd683e1351d2c59
Adam Axe <adam_axe@mastercard.com>
2014-05-21 11:23:37 -0500

    resized cupcake to make dl reasonable MAPPS-2939

--- SimplifySDKSampleApp/SimplifySDKSampleApp/SIMProductViewController.m
+++ SimplifySDKSampleApp/SimplifySDKSampleApp/SIMProductViewController.m
@@ -5,6 +5,7 @@
 #import <Simplify/UIColor+Simplify.h>
 #import <Simplify/SIMResponseViewController.h>
 
+//1. Sign up to be a SIMChargeViewControllerDelegate so that you get the callback that gives you a token
 @interface SIMProductViewController ()<SIMChargeCardViewControllerDelegate>
 @property (nonatomic, strong) SIMChargeCardViewController *chargeController;
 @property (strong, nonatomic) IBOutlet SIMButton *buyButton;
@@ -29,11 +30,15 @@
 
 #pragma mark - Navigation
 - (IBAction)buyCupcake:(id)sender {
+
+    //2. Create a SIMChargeViewController with your public api key
     SIMChargeCardViewController *chargeController = [[SIMChargeCardViewController alloc] initWithPublicKey:@"sbpb_N2ZkOGIwZWYtYTg3My00OTE1LWI3ZjgtMzZhMzZhZTAyYTY5" primaryColor:self.primaryColor];
 
+    //3. Assign your class as the delegate to the SIMChargeViewController class which takes the user input and requests a token
     chargeController.delegate = self;
     self.chargeController = chargeController;
-    
+
+    //4. Add SIMChargeViewController to your view hierarchy
     [self presentViewController:self.chargeController animated:YES completion:nil];
     
 }
@@ -57,6 +62,7 @@
     [self presentViewController:viewController animated:YES completion:nil];
 }
 
+//5. This method will be called on your class whenever the user presses the Charge Card button and tokenization succeeds
 -(void)creditCardTokenProcessed:(SIMCreditCardToken *)token {
     //Token was generated successfully, now you must use it
     
