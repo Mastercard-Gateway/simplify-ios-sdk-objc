e8eae57f0768005ecb16f6a53d051b488e4af444
Adam Axe <adam_axe@mastercard.com>
2014-05-21 12:52:13 -0500

    docs cleanup and inclusion MAPPS-2939

--- SimplifyMerchantSDK/SimplifyMerchantSDK/SIMChargeCardViewController.h
+++ SimplifyMerchantSDK/SimplifyMerchantSDK/SIMChargeCardViewController.h
@@ -1,3 +1,26 @@
+/**
+ @mainpage
+ Simplify by Mastercard iOS SDK. Start taking payments today.
+ 
+ @section intro_sec Introduction
+ You can use this SDK to generate a card token for a payment. You can either utilize the SIMChargeCardViewController to handle the complete lifecycle of user input to token generation, or use the SIMSimplify class to generate it yourself.
+
+ @section implementation_sec Implementation
+
+ You can implement the SDK using three different mechanisms: SIMChargeCardViewController,  SIMChargeCardModel and SIMSimplify.
+ <br/><br/>
+ SIMChargeCardViewController and SIMChargeCardViewControllerDelegate - This is the turn key solution. Simply create a SIMChargeCardViewController, signup as a SIMChargeCardViewControllerDelegate and implement the callbacks. All card input validation and token creation is handled for you. You simply present the UIViewController subclass and wait for for the token
+ <br/><br/>
+ SIMChargeCardModel and SIMChargeCardModelDelegate - This model allows you to input the fields required to make a payment, validates them and then allows you to retrieve a token. Create a SIMChargeCardModel, signup as a SIMChargeCardModelDelegate, implement the callbacks and then ask the SIMChargeCardModel to retrieveToken.
+ <br/><br/>
+ SIMSimplify - This object allows you to pass in the parameters required for tokenization and a completionHandler that will either return the token or an error.
+ 
+ @class SIMChargeCardViewController
+ 
+ @author Copyright (c) 2014 Mastercard International Incorporated. All Rights Reserved.
+ @file
+ */
+
 #import <UIKit/UIKit.h>
 #import "SIMCreditCardToken.h"
 
@@ -24,9 +47,6 @@
 
 @end
 
-/**
- View Controller that has a SIMChargeCardModel that validates the three fields: credit card number, expiration date, and CVC code
- */
 @interface SIMChargeCardViewController : UIViewController
 
 -(instancetype)initWithPublicKey:(NSString *)publicKey;
