bf4442bade3798636f4e11e50ea4990991a628f8
Neem Serra <neem_serra@mastercard.com>
2014-06-24 14:51:21 -0500

    adding country into address MAPPS-3130

--- SimplifyMerchantSDK/SimplifyMerchantSDK/SIMAddressTests.m
+++ SimplifyMerchantSDK/SimplifyMerchantSDK/SIMAddressTests.m
@@ -25,9 +25,21 @@
 	XCTAssertTrue([self.testSubject.country isEqualToString:@"US"]);
 }
 
+-(void)testThatAddressCanBeCreatedWithCountry {
+	self.testSubject = [[SIMAddress alloc] initWithName:@"Sam Simple" addressLine1:@"2200 Mastercard Blvd" addressLine2:@"Area 1" city:@"O'Fallon" state:@"MO" country:@"Canada" zip:@"63368"];
+    
+	XCTAssertTrue([self.testSubject.name isEqualToString:@"Sam Simple"]);
+	XCTAssertTrue([self.testSubject.addressLine1 isEqualToString:@"2200 Mastercard Blvd"]);
+	XCTAssertTrue([self.testSubject.addressLine2 isEqualToString:@"Area 1"]);
+	XCTAssertTrue([self.testSubject.city isEqualToString:@"O'Fallon"]);
+	XCTAssertTrue([self.testSubject.state isEqualToString:@"MO"]);
+	XCTAssertTrue([self.testSubject.zip isEqualToString:@"63368"]);
+	XCTAssertTrue([self.testSubject.country isEqualToString:@"Canada"]);
+}
+
 -(void)testThatAddressFieldsWithNilAreSafelyConvertedToEmptyStrings {
 
-    self.testSubject = [[SIMAddress alloc] initWithName:nil addressLine1:nil addressLine2:nil city:nil state:nil zip:nil];
+    self.testSubject = [[SIMAddress alloc] initWithName:nil addressLine1:nil addressLine2:nil city:nil state:nil country:nil zip:nil];
 
 	XCTAssertNotNil(self.testSubject.name);
 	XCTAssertNotNil(self.testSubject.addressLine1);
